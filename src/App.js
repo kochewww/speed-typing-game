ca7f23c1522e956219833377a0a255d0e2c02fc3
kochewww <vladusta2@gmail.com>
2019-12-24 15:41:27 +0300

    word counter has been added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+  function countWord(text) {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter(word => word !== " ").length;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>Time reminaing: ???</h4>
+      <button onChange={handleChange} value={text}>
+        Start
+      </button>
+      <h1>Word count: ???</h1>
     </div>
   );
 }
