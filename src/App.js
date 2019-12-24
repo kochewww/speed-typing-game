2728ab3d90a047c95fde6a91498bafa8eb6c0bdd
kochewww <vladusta2@gmail.com>
2019-12-24 17:18:49 +0300

    Timer has been added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState("10");
+
   function handleChange(e) {
     const { value } = e.target;
     setText(value);
@@ -11,11 +13,18 @@ function App() {
     const wordsArr = text.trim().split(" ");
     return wordsArr.filter(word => word !== " ").length;
   }
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea />
-      <h4>Time reminaing: ???</h4>
+      <h4>Time reminaing: {timeRemaining}</h4>
       <button onChange={handleChange} value={text}>
         Start
       </button>
