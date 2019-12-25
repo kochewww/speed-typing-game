028525e5ec9b3b87b442a9011361dedaccb52b2f
kochewww <vladusta2@gmail.com>
2019-12-25 11:03:54 +0300

    timer now starts with the button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState("10");
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -14,18 +15,24 @@ function App() {
     return wordsArr.filter(word => word !== " ").length;
   }
   useEffect(() => {
-    if (timeRemaining > 0) {
+    if (timeRemaining > 0 && isTimeRunning) {
       setTimeout(() => {
         setTimeRemaining(time => time - 1);
       }, 1000);
+    } else if (timeRemaining === 0) {
+      setIsTimeRunning(false);
     }
-  }, [timeRemaining]);
+  }, [timeRemaining, isTimeRunning]);
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea />
       <h4>Time reminaing: {timeRemaining}</h4>
-      <button onChange={handleChange} value={text}>
+      <button
+        onChange={handleChange}
+        onClick={() => setIsTimeRunning(true)}
+        value={text}
+      >
         Start
       </button>
       <h1>Word count: ???</h1>
